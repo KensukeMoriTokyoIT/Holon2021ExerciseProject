2c2e865106c83760db52574e8aa20566ea3ce102
sh-yamada <s-yamada@holon.co.jp>
2021-04-19 16:46:45 +0900

    管理者更新サーブレット

--- Dekopon/web_crud_sample/src/jp/co/sss/emanage/manage/UpdateInputServlet.java
+++ Dekopon/web_crud_sample/src/jp/co/sss/emanage/manage/UpdateInputServlet.java
@@ -1,11 +1,17 @@
 package jp.co.sss.emanage.manage;
 
 import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import jp.co.sss.emanage.bean.EmpBean;
+import jp.co.sss.emanage.dao.EmpDao;
+import jp.co.sss.emanage.form.UpdateForm;
 
 /**
  * Servlet implementation class UpdateInputServlet
@@ -13,29 +19,33 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/UpdateInputServlet")
 public class UpdateInputServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public UpdateInputServlet() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
+
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public UpdateInputServlet() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
 
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
-	}
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		UpdateForm updateForm = new UpdateForm();
+		//DBからID検索
+		EmpBean empBean = EmpDao.findById(request.getParameter("empId"));
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
-	}
+		updateForm.setEmpName(empBean.getEmpName());
+		updateForm.setGender(empBean.getGender());
+		updateForm.setAddress(empBean.getAddress());
+		updateForm.setBirthday(empBean.getBirthday());
 
+		request.setAttribute("updateForm", updateForm);
+
+		RequestDispatcher dispatcher = request
+                .getRequestDispatcher("jsp/update/update.jsp");
+        dispatcher.forward(request, response);
+	}
 }
