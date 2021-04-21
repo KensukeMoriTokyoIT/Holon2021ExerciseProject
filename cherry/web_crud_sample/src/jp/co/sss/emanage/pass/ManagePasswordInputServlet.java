79a99c02dd0488f2e462b060b96ced0c0b1cff90
牧野光平 <k-makino@holon.co.jp>
2021-04-21 16:10:48 +0900

    パスワード変更

---
+++ cherry/web_crud_sample/src/jp/co/sss/emanage/pass/ManagePasswordInputServlet.java
@@ -0,0 +1,35 @@
+package jp.co.sss.emanage.pass;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+/**
+ * Servlet implementation class ManagePasswordChangeServlet
+ */
+@WebServlet("/ManagePasswordInputServlet")
+public class ManagePasswordInputServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public ManagePasswordInputServlet() {
+        super();
+    }
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+       	// 管理者用パスワード変更画面へ遷移を行う
+         RequestDispatcher dispatcher = request
+                        .getRequestDispatcher("/jsp/password/manage_pass.jsp");
+                dispatcher.forward(request, response);
+            }
+}
