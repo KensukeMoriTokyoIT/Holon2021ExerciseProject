25b4be39c7b5dc005f6d761efc00767e9dc7bb12
oobayashi-ikuto <iueo.bayashi@gmail.com>
2021-04-21 15:32:50 +0900

    password/ManagePasswordInputServlet.classコミット

---
+++ Acerola/web_crud_sample/src/password/ManagePasswordInputServlet.java
@@ -0,0 +1,20 @@
+package password;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class ManagePasswordInputServlet
+ */
+@WebServlet("/ManagePasswordInputServlet")
+public class ManagePasswordInputServlet extends HttpServlet {
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		request.getRequestDispatcher("/jsp/password/PasswordManage.jsp").forward(request, response);
+	}
+	}
