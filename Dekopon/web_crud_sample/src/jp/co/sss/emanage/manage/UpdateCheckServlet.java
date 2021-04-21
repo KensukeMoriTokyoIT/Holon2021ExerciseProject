5e814435804812cdadc7d1bb0a4d26ebbab381f5
sh-yamada <s-yamada@holon.co.jp>
2021-04-21 11:42:48 +0900

    一般社員パスワード変更修正、管理者更新修正 commit

--- Dekopon/web_crud_sample/src/jp/co/sss/emanage/manage/UpdateCheckServlet.java
+++ Dekopon/web_crud_sample/src/jp/co/sss/emanage/manage/UpdateCheckServlet.java
@@ -1,6 +1,8 @@
 package jp.co.sss.emanage.manage;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -10,6 +12,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import jp.co.sss.emanage.form.UpdateForm;
+import jp.co.sss.emanage.util.InputValidator; //入力チェック
 
 /**
  * Servlet implementation class UpdateCheckServlet
@@ -18,42 +21,86 @@ import jp.co.sss.emanage.form.UpdateForm;
 public class UpdateCheckServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public UpdateCheckServlet() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public UpdateCheckServlet() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		//入力チェック用クラス
+		InputValidator iv = new InputValidator();
+		String error = new String();
+		List<String> errorMessages = new ArrayList<>();
+
+		//アップデートフォーム
 		UpdateForm updateForm = new UpdateForm();
 
-		String empId = request.getParameter("empId"); //社員ID
-		String empPass = request.getParameter("empPass"); //パスワード
-		String empName = request.getParameter("empName"); //名前
-		String gender = request.getParameter("gender"); //性別
-		String address = request.getParameter("address"); //住所
-		String birthday = request.getParameter("birthday"); //生年月日
-		String authority = request.getParameter("authority"); //権限
-		String deptId = request.getParameter("deptId"); //部署ID
-
+		//社員ID
+		String empId = request.getParameter("empId");
 		updateForm.setEmpId(empId);
+		//パスワード
+		String empPass = request.getParameter("empPass");
 		updateForm.setEmpPass(empPass);
+		if ((error = iv.passwordValidate(empPass)) != null) {
+			errorMessages.add(error);
+		}
+		//名前
+		String empName = request.getParameter("empName");
 		updateForm.setEmpName(empName);
+		if ((error = iv.nameValidate(empName)) != null) {
+			errorMessages.add(error);
+		}
+		//性別
+		String gender = request.getParameter("gender");
 		updateForm.setGender(gender);
+		if ((error = iv.genderValidate(gender)) != null) {
+			errorMessages.add(error);
+		}
+		//住所
+		String address = request.getParameter("address");
 		updateForm.setAddress(address);
+		if ((error = iv.addressValidate(address)) != null) {
+			errorMessages.add(error);
+		}
+		//生年月日
+		String birthday = request.getParameter("birthday");
 		updateForm.setBirthday(birthday);
+		if ((error = iv.birthdayValidate(birthday)) != null) {
+			errorMessages.add(error);
+		}
+		//権限
+		String authority = request.getParameter("authority");
 		updateForm.setAuthority(authority);
+		if ((error = iv.authorityValidate(authority)) != null) {
+			errorMessages.add(error);
+		}
+		//部署ID
+		String deptId = request.getParameter("deptId");
 		updateForm.setDeptId(deptId);
+		if ((error = iv.deptIdValidate(deptId)) != null) {
+			errorMessages.add(error);
+		}
 
-		request.setAttribute("updateForm", updateForm);
-		RequestDispatcher dispatcher = request
-                .getRequestDispatcher("jsp/update/check.jsp");
-        dispatcher.forward(request, response);
+		//入力チェック
+		if (errorMessages.isEmpty()) {
+			request.setAttribute("updateForm", updateForm);
+			RequestDispatcher dispatcher = request
+					.getRequestDispatcher("jsp/update/check.jsp");
+			dispatcher.forward(request, response);
+		} else {
+			//エラーあり
+			request.setAttribute("errorMessages", errorMessages);
+			RequestDispatcher dispatcher = request
+					.getRequestDispatcher("jsp/update/update.jsp");
+			dispatcher.forward(request, response);
+		}
 
 	}
 
