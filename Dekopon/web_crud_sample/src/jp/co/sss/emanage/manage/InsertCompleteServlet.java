eb34eb45c674b24d63fd36c1ecb113f4eb0c4c9a
amii1231 <a-iiduka@holon.co.jp>
2021-04-21 19:59:17 +0900

    insertcompleteservlet

--- Dekopon/web_crud_sample/src/jp/co/sss/emanage/manage/InsertCompleteServlet.java
+++ Dekopon/web_crud_sample/src/jp/co/sss/emanage/manage/InsertCompleteServlet.java
@@ -8,12 +8,16 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import jp.co.sss.emanage.bean.EmpBean;
+import jp.co.sss.emanage.dao.EmpDao;
+
 /**
  * Servlet implementation class InsertCompleteServlet
  */
 @WebServlet("/InsertCompleteServlet")
 public class InsertCompleteServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
+	private EmpBean EmpBean;
 
     /**
      * @see HttpServlet#HttpServlet()
@@ -23,20 +27,15 @@ public class InsertCompleteServlet extends HttpServlet {
         // TODO Auto-generated constructor stub
     }
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
-	}
+
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
+
+		EmpDao.insert(EmpBean);
+	 request.getRequestDispatcher("/jsp/Insert/insertcomplete.jsp").forward(request, response);
 	}
 
 }
