aed854ea41b56d1d0ad6d6c7f8508159ccd7de0b
mimuuuu <pastel917@gmail.com>
2021-04-22 13:29:26 +0900

    アセロラチームのDelete関連ファイル

---
+++ Acerola/web_crud_sample/src/delete/Delete.java
@@ -0,0 +1,21 @@
+package delete;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import jp.co.sss.emanage.dao.*;
+
+@WebServlet("/Delete")
+public class Delete extends HttpServlet {
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		String empId = request.getParameter("empId");
+		EmpDao.delete(empId);
+		request.getRequestDispatcher("/jsp/delete/employee_complete.jsp").
+		forward(request,response);
+	}
+}
